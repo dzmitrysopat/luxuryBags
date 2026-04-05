d39386f9dd08f4309387534b52efce50cdd72be1
dzmitrysopat <102973988+dzmitrysopat@users.noreply.github.com>
2026-04-05 19:22:16 +0300

    main

---
+++ script.js
@@ -0,0 +1,9 @@
+        card.innerHTML = `
+            <article class="card">
+                <img src="" alt="">
+                <span class="name"></span>
+                <span class="price"></span>
+            </article>
+        `;
+
+        container.appendChild(card);
