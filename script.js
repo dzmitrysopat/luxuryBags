cfc1ee43337901b317d49a6366698712803f2730
dzmitrysopat <102973988+dzmitrysopat@users.noreply.github.com>
2026-05-05 22:23:35 +0300

    main

--- script.js
+++ script.js
@@ -268,12 +268,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-let imgs = [
-        // './images/banner/043.PNG',
-        // './images/banner/banner_03.jpg',
-        // './images/banner/banner_06.jpg',
-        './images/banner/banner_01.jpg',
-        ]; //   Добавьте свои картинки через запятую
-        let banner = document.getElementById("banner");
-    let randomIndex = Math.floor(Math.random() * imgs.length);
-    banner.style.backgroundImage = `url('${imgs[randomIndex]}')`;
+// let imgs = [
+//         './images/banner/banner_01.jpg',
+//         ];
+//         let banner = document.getElementById("banner");
+//     let randomIndex = Math.floor(Math.random() * imgs.length);
+//     banner.style.backgroundImage = `url('${imgs[randomIndex]}')`;
